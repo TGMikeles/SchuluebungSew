66fe18b3a6c36850269bc3e5f6c56f98786affb7
TGMikeles <ikeles@student.tgm.ac.at>
2016-04-14 11:41:57 +0200

    Added files via upload

---
+++ Bevarage.java
@@ -0,0 +1,11 @@
+
+public abstract class Bevarage {
+	private String description ="Ich weiﬂ es noch nicht";
+
+	public String getDescription()
+	{	
+		return description;
+	}
+	public abstract double cost();
+}
+
---
+++ Espresso.java
@@ -0,0 +1,10 @@
+
+public class Espresso extends Bevarage{
+
+	@Override
+	public double cost() {
+		
+		return 1.99;
+	}
+
+}
